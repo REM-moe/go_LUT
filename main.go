b2e7230d53ca71307f4cea783672b207cf82adb4
rem <abhaykv111@gmail.com>
2026-02-07 06:36:32 +0530

    feat: update LUT data structure and improve loadLUT function  to load LUT data into a struct

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type Lut3D struct {
 	Title string
 	Size  int
-	Data  [][][]Color
+	Data  []Color
 }
 
 type Color struct {
@@ -28,9 +31,60 @@ func main() {
 	imagesDir := os.Args[1]
 	lutFile := os.Args[2]
 
-	fmt.Printf("**************** \n FOLDER : %s  \n**************** \n LUT FILE : %s", imagesDir, lutFile)
+	fmt.Printf(" FOLDER : %s \n LUT FILE : %s\n", imagesDir, lutFile)
+
+	loadLUT(lutFile)
 }
 
-func processImage(image string) (*Lut3D, error) {
-	return nil, nil
+func loadLUT(filename string) (*Lut3D, error) {
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	lut := &Lut3D{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := strings.Fields(line)
+
+		// check word starts with LUT_3D_SIZE
+		if strings.Contains(line, "LUT_3D_SIZE") && len(words) >= 2 {
+
+			text, err := strconv.Atoi(words[1])
+
+			if err != nil {
+				return nil, err
+			}
+
+			lut.Size = text
+			lut.Data = make([]Color, 0, text*text*text)
+
+		} else if len(words) == 3 {
+			r, err1 := strconv.ParseFloat(words[0], 64)
+			g, err2 := strconv.ParseFloat(words[1], 64)
+			b, err3 := strconv.ParseFloat(words[2], 64)
+
+			if err1 != nil || err2 != nil || err3 != nil {
+				return nil, fmt.Errorf("bad color data: %s", line)
+			}
+
+			lut.Data = append(lut.Data, Color{
+				R: r,
+				G: g,
+				B: b,
+			})
+
+		} else {
+			continue
+		}
+	}
+
+	return lut, nil
+
 }
